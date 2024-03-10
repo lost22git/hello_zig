5285da42a66c743c275f98b80ec6c54edd40f9dd
lost <lost>
2024-03-11 03:31:36 +0800

    add fn.zig

--- fn.zig
+++ fn.zig
@@ -1,3 +1,36 @@
 ///usr/bin/env zig test -freference-trace "$0" "$@" ; exit $?
 const std = @import("std");
 const testing = std.testing;
+
+fn ptrArgs(a: *u32) void {
+    _ = a;
+}
+
+fn ptrArgs2(a: *const u32) void {
+    _ = a;
+}
+
+fn functionPtrArgs(f: *const fn (x: u32) void) *const fn (x: u32) void {
+    return f;
+}
+
+fn genericArgs(T: type, a: T) T {
+    _ = a;
+}
+
+fn anytypeArgs(a: anytype) void {
+    _ = a;
+}
+
+fn anytypeArgs2(a: anytype, b: @TypeOf(a)) void {
+    _ = b;
+}
+
+fn inferReturnTypeByArgs(a: anytype) switch (@typeInfo(@TypeOf(a))) {
+    .Pointer => |ptr| ptr.child,
+    else => void,
+} {}
+
+fn comptimeArgs(comptime a: []const u8) void {
+    _ = a;
+}
