afc544396110229d854911ba206b6d3cf98a0d25
lost <lost>
2024-03-11 07:17:53 +0800

    update fn.zig

--- fn.zig
+++ fn.zig
@@ -39,3 +39,15 @@ fn inferReturnTypeByArgs(a: anytype) switch (@typeInfo(@TypeOf(a))) {
 fn comptimeArgs(comptime a: []const u8) void {
     _ = a;
 }
+
+fn namedArgs(a: struct { cmd: []const u8, args: []const []const u8 = &[_][]const u8{} }) @TypeOf(a) {
+    return a;
+}
+
+test "named args" {
+    const args = [_][]const u8{"version"};
+    const cmdArgs = namedArgs(.{ .cmd = "zig", .args = &args });
+
+    try testing.expectEqualStrings("zig", cmdArgs.cmd);
+    try testing.expectEqualSlices([]const u8, &args, cmdArgs.args);
+}
