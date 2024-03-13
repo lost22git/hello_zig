fb64708756c0025189d6d37138cea06b472e26c2
lost <lost>
2024-03-13 10:06:09 +0800

    optimize process.zig

--- process.zig
+++ process.zig
@@ -32,7 +32,7 @@ test "run cmd success" {
 
     const result = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ "zig", "zen" },
+        .argv = &.{ "zig", "zen" },
     });
     defer allocator.free(result.stdout);
     defer allocator.free(result.stderr);
@@ -47,7 +47,7 @@ test "run cmd failed" {
 
     const result = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ "zig", "-asdfak" },
+        .argv = &.{ "zig", "-asdfak" },
     });
     defer allocator.free(result.stdout);
     defer allocator.free(result.stderr);
@@ -60,7 +60,7 @@ test "run cmd failed" {
 // test "run cmd redirect stderr to stdout" {
 //     const allocator = testing.allocator;
 //
-//     var p = std.process.Child.init(&[_][]const u8{ "zig", "-asdfak" }, allocator);
+//     var p = std.process.Child.init(&.{ "zig", "-asdfak" }, allocator);
 //     p.stdout_behavior = .Pipe;
 //     p.stderr = p.stdout; // NOTE: not works: redirect stderrr to stdout
 //
@@ -78,7 +78,7 @@ test "run cmd failed" {
 test "run cmd not found" {
     const allocator = testing.allocator;
 
-    const cmdResult = std.process.Child.run(.{ .allocator = allocator, .argv = &[_][]const u8{"zigg"} });
+    const cmdResult = std.process.Child.run(.{ .allocator = allocator, .argv = &.{"zigg"} });
     try testing.expectError(error.FileNotFound, cmdResult);
 }
 
@@ -90,8 +90,8 @@ test "run shell cmd" {
     const cmd = "zig -sdfs 2>&1";
 
     const shellCmd = switch (builtin.os.tag) {
-        .windows => [_][]const u8{ "pwsh", "-NoProfile", "-NoLogo", "-Command", cmd },
-        else => [_][]const u8{ "sh", "-c", cmd },
+        .windows => .{ "pwsh", "-NoProfile", "-NoLogo", "-Command", cmd },
+        else => .{ "sh", "-c", cmd },
     };
 
     const result = try std.process.Child.run(.{
